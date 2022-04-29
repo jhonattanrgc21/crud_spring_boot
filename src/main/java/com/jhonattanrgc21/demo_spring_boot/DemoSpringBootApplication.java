5b1885e59e02df6b96520e62557a35b9d6b9d40a
Jhonattan Garcia <jgarciac@intelix.biz>
2022-04-29 14:53:44 -0400

    Agregue la configuracion para el swagger de jsondoc

--- src/main/java/com/jhonattanrgc21/demo_spring_boot/DemoSpringBootApplication.java
+++ src/main/java/com/jhonattanrgc21/demo_spring_boot/DemoSpringBootApplication.java
@@ -1,9 +1,11 @@
 package com.jhonattanrgc21.demo_spring_boot;
 
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJSONDoc
 public class DemoSpringBootApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jhonattanrgc21/demo_spring_boot/controllers/UserController.java
+++ src/main/java/com/jhonattanrgc21/demo_spring_boot/controllers/UserController.java
@@ -16,6 +16,7 @@ import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/user")
+//@CrossOrigin("*")
 public class UserController {
     @Autowired
     private UserServices userServices;
